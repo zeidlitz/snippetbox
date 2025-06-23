d89cf1630917e539939a6b47acd913a7cf8462b5
zeidlitz <zeidlitzerik@gmail.com>
2025-06-23 12:20:38 +0200

    Executing SQL statements

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -38,7 +38,17 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Display a form for creating a new snippet")
+	title := "0 snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
