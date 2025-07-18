a89a8bf0f79779e3d656037c4027724f11cbd50f
zeidlitz <zeidlitzerik@gmail.com>
2025-07-18 12:04:10 +0200

    Catching Runtime Errors while rendering templates

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 )
@@ -26,11 +27,15 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 		app.serverError(w, r, err)
 		return
 	}
-	w.WriteHeader(status)
 
-	err := ts.ExecuteTemplate(w, "base", data)
+	buf := new(bytes.Buffer)
+
+	err := ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
+		return
 	}
 
+	w.WriteHeader(status)
+	buf.WriteTo(w)
 }
