9ec50ef13ced31343984c7aa8be88d3f0ed4ca0b
zeidlitz <zeidlitzerik@gmail.com>
2025-08-01 11:06:05 +0200

    Automatic Form Parsing

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/zeidlitz/snippetbox/internal/models"
 
+	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -17,6 +18,7 @@ type application struct {
 	logger        *slog.Logger
 	snippets      *models.SnippetModel
 	templateCache map[string]*template.Template
+	formDecoder   *form.Decoder
 }
 
 func main() {
@@ -39,10 +41,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	formDecoder := form.NewDecoder()
+
 	app := &application{
 		logger:        logger,
 		snippets:      &models.SnippetModel{DB: db},
 		templateCache: templateCache,
+		formDecoder:   formDecoder,
 	}
 
 	logger.Info("Starting server", "addr", *addr)
