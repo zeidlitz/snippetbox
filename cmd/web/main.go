9187196f0de0d2c070eb588c1dbbd17b3bf7fd34
zeidlitz <zeidlitzerik@gmail.com>
2025-08-06 22:43:23 +0200

    Security Settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"crypto/tls"
 	"flag"
 	"html/template"
 	"log/slog"
@@ -59,10 +60,19 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+
+
 	srv := &http.Server{
 		Addr:     *addr,
 		Handler:  app.routes(),
 		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		TLSConfig: tlsConfig,
+		IdleTimeout: time.Minute,
+		ReadTimeout: 5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	logger.Info("Starting server", "addr", *addr)
@@ -71,7 +81,6 @@ func main() {
 	os.Exit(1)
 }
 
-// Wraps sql.Open() and returns a sql.DB connection pool for a given DSNmain
 func openDB(dsn string) (*sql.DB, error) {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
