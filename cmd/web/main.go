a3307d1d53ae741f9e9b591453429c98be6beed0
zeidlitz <zeidlitzerik@gmail.com>
2025-08-06 22:30:58 +0200

    Enable HTTPS

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -49,6 +49,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		logger:         logger,
@@ -65,7 +66,7 @@ func main() {
 	}
 
 	logger.Info("Starting server", "addr", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	logger.Error(err.Error())
 	os.Exit(1)
 }
